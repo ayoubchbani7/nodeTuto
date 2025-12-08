6ce73cc6728f7244462f95a3d1efba34d71030f6
achbani9 <a.chbani@eca-assurances.com>
2025-12-08 16:10:34 +0100

    Add getUniqueId function and update dependencies

--- helper.js
+++ helper.js
@@ -1,3 +1,10 @@
 export const success = (message, data) => {
     return { message, data };
 };
+
+export const getUniqueId = (pokemons) => {
+    const pokemonIds = pokemons.map(pokemon => pokemon.id);
+    const maxId = pokemonIds.reduce((a,b)=>Math.max(a,b))
+    const uniqueId = maxId +1 ;
+    return uniqueId;
+}
